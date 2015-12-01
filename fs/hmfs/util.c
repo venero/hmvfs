2e2c11839ea61c62876811ea9f8524b2915d8bdc
wulinzhu <wulinzhu1954@gmail.com>
2015-12-01 15:03:19 +0800

    Add Fast-Read for read-only file: When an file is open with O_RDONLY mode, I would search an large enough VMALLOC area, and remap all the data block of the file into that area. In the implementation, I use some extern function declare by other file in kernel, such as init_mm, get_vm_area, remove_vm_area and etc. However, I have no idea how to refer to those function in formal way. Therefore, I just search the kernel system map in loading modules. I think this is unsafe. And maybe we could find a better method later. Finnaly, fast-read is configurable.

---
+++ fs/hmfs/util.c
@@ -0,0 +1,55 @@
+#include <linux/kallsyms.h>
+#include "util.h"
+#include "hmfs.h"
+
+/* 
+ * FIXME: Any good idea to call the function which is not exported
+ * by macro EXPORT_SYMBOL
+ */
+int init_util_function(void)
+{
+	unsigned long sym_addr = 0;
+
+	sym_addr = kallsyms_lookup_name("init_mm");
+	if (!sym_addr)
+		goto fail;
+	hmfs_init_mm = (struct mm_struct *)sym_addr;
+
+	sym_addr = kallsyms_lookup_name("pud_alloc");
+	if (!sym_addr)
+		goto fail;
+	hmfs_pud_alloc = 
+			(pud_t * (*) (struct mm_struct *, pgd_t *, unsigned long))sym_addr;
+
+	sym_addr = kallsyms_lookup_name("pmd_alloc");
+	if (!sym_addr)
+		goto fail;
+	hmfs_pmd_alloc = 
+			(pmd_t * (*) (struct mm_struct *, pud_t *, unsigned long))sym_addr;
+
+	sym_addr = kallsyms_lookup_name("__pte_alloc_kernel");
+	if (!sym_addr)
+		goto fail;
+	__hmfs_pte_alloc_kernel = (int (*) (pmd_t *, unsigned long))sym_addr;
+
+	sym_addr = kallsyms_lookup_name("remove_vm_area");
+	if (!sym_addr)
+		goto fail;
+	hmfs_remove_vm_area = (struct vm_struct * (*) (const void *))sym_addr;
+
+	sym_addr = kallsyms_lookup_name("find_vm_area");
+	if (!sym_addr)
+		goto fail;
+	hmfs_find_vm_area = (struct vm_struct * (*) (const void *))sym_addr;
+
+	sym_addr = kallsyms_lookup_name("get_vm_area");
+	if (!sym_addr)
+		goto fail;
+	hmfs_get_vm_area = 
+			(struct vm_struct * (*) (unsigned long, unsigned long))sym_addr;
+
+	return 0;
+fail:
+	printk(KERN_INFO"[HMFS]: Fail to get all needed function\n");
+	return -EPERM; 
+}
